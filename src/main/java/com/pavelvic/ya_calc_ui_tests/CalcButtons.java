e12884895d3aeb6701b6fcef1b869208aaa5ea51
pavelvic <pavelvic@gmail.com>
2022-05-12 14:46:06 +0300

    Refactoring manual input test

---
+++ src/main/java/com/pavelvic/ya_calc_ui_tests/CalcButtons.java
@@ -0,0 +1,18 @@
+package com.pavelvic.ya_calc_ui_tests;
+
+public enum CalcButtons {
+    NULL,
+    ONE,
+    TWO,
+    FOUR,
+    FIVE,
+    SEP,
+    PI,
+    SQRT,
+    COS,
+    MULTIPLY,
+    DIV,
+    BRACKETS,
+    CLEAR,
+    EQUAL
+}
